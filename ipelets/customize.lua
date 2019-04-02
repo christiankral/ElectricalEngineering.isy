fa75e2a45f515086c799749731ba3acbd187cd44
Christian Kral <dr.christian.kral@gmail.com>
2019-04-02 19:21:38 +0200

    Re-phrase documentation of ipelet

--- ipelets/customize.lua
+++ ipelets/customize.lua
@@ -1,6 +1,6 @@
 -- shortcuts.ipelet_2_symbols = "Alt+Y", -- Use symbol
 ----------------------------------------------------------------------
--- My customization ipelet: customize.lua
+-- Customized ipelet: customize.lua
 ----------------------------------------------------------------------
 prefs.max_zoom = 100
 shortcuts.ipelet_1_symbols = "Shift+S"
