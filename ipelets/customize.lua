ece98b9dae959a3ac58b74bf4ca2d976e070e755
Christian Kral <dr.christian.kral@gmail.com>
2019-04-02 19:05:49 +0200

    Add customized ipelet

---
+++ ipelets/customize.lua
@@ -0,0 +1,8 @@
+-- shortcuts.ipelet_2_symbols = "Alt+Y", -- Use symbol
+----------------------------------------------------------------------
+-- My customization ipelet: customize.lua
+----------------------------------------------------------------------
+prefs.max_zoom = 100
+shortcuts.ipelet_1_symbols = "Shift+S"
+shortcuts.mode_splines = "Alt+Ctrl+I"
+prefs.scroll.vertical_sign = -1
