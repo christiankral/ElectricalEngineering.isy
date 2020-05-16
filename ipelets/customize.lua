4c3c6ec74b137882b6b52f198c9d10ac4808c2d4
Christian Kral <dr.christian.kral@gmail.com>
2020-05-16 22:27:12 +0200

    Update costomize.lua

--- ipelets/customize.lua
+++ ipelets/customize.lua
@@ -5,4 +5,5 @@ prefs.max_zoom = 100
 shortcuts.ipelet_1_symbols = "Shift+S"
 shortcuts.mode_splines = "Alt+Ctrl+I"
 prefs.scroll.vertical_sign = -1
-prefs.styles = { "/path/to/ElectricalEngineering.isy" }
+-- Substitute with /path/to/ElectricalEngineering.isy
+prefs.styles = { "~/.ipe/styles/ElectricalEngineering.isy" }
