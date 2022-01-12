beb42d771ef00f4611b1f37838f35b8c8a70c976
Christian Kral <dr.christian.kral@gmail.com>
2022-01-12 18:39:26 +0100

    Add shortcuts of text alignment

--- ipelets/customize.lua
+++ ipelets/customize.lua
@@ -5,5 +5,15 @@ prefs.max_zoom = 100
 shortcuts.ipelet_1_symbols = "Shift+S"
 shortcuts.mode_splines = "Alt+Ctrl+I"
 prefs.scroll.vertical_sign = -1
--- Substitute with /path/to/ElectricalEngineering.isy
+
+shortcuts["horizontalalignment|left"] = "Alt+Shift+L"
+shortcuts["horizontalalignment|hcenter"] = "Alt+Shift+H"
+shortcuts["horizontalalignment|right"] = "Alt+Shift+R"
+
+shortcuts["verticalalignment|bottom"] = "Alt+Shift+B"
+shortcuts["verticalalignment|baseline"] = "Alt+B"
+shortcuts["verticalalignment|vcenter"] = "Alt+Shift+C"
+shortcuts["verticalalignment|top"] = "Alt+Shift+T"
+
+-- Substitute with absolute /path/to/ElectricalEngineering.isy
 prefs.styles = { "~/.ipe/styles/ElectricalEngineering.isy" }
